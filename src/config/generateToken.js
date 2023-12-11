fe4e62c811e3a18ac714bba7244cc1f659ceda1f
Vinícius Lima <viniciusraflima@gmail.com>
2023-12-11 18:00:19 -0300

    chore: change files from config folder to utils

--- src/config/generateToken.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-
-const jwt = require('jsonwebtoken')
-
-module.exports = (params) => jwt.sign(params, process.env.JWTSECRET, {
-    expiresIn: 86400,
-})
--- src/config/permissionRule.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    admin: 1,
-    user: 2
-}
